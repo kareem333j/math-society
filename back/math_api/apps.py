51fe290ed418f811b798add2308997036b84715d
karem <karemmagdy247@gmail.com>
2024-11-13 22:25:52 +0200

    update

--- back/math_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MathApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'math_api'
--- back/users/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . models import NewUser, Profile
-
-admin.site.register(NewUser)
-admin.site.register(Profile)
