3c36321e56a28563547a4962f8c4f556a107a02b
karem <karemmagdy247@gmail.com>
2025-02-21 22:55:59 +0200

    Update Axios.js

--- front/src/Axios.js
+++ front/src/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const domain = 'http://127.0.0.1:8000';
+export const domain = 'https://math-api002.vercel.app'
 export const baseURLMedia = `${domain}/media/`;
 export const baseURLMediaTypeImage = `${domain}`;
 export const baseURL = `${domain}/api`;
